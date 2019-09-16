dad9d0a60fed3b001c14fda381837b040e00ae08
NJ SUNG <nakjunizm@gmail.com>
2019-09-16 15:15:26 +0900

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const router = express.Router();
 //const path = require('path');
 
 const app = express();
 
 connectDB();
+
+router.get("/", async (req, res) => {
+  res.status(200).send(`It's up and running!`);
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
